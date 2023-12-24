32326b175969cccad0148127c9ce427c454cb2ec
Cotchi666 <chien.170901@gmail.com>
2023-12-24 18:53:27 +0700

    create 1st route home

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return 'Hello, Flask!'
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
 
 
-# if __name__ == '__main__':
-#         app.run()
+if __name__ == '__main__':
+        app.run()
 
-msg= "Chien Not Toxic"
-print(msg)
+# msg= "Chien Not Toxic"
+# print(msg)
